395a2a4f397750763754b4e72e1f90bbfc266ec5
Matthias Aarnoutse <mattiejas@outlook.com>
2021-07-02 20:33:02 +0200

    Reduce size to 179 bytes

--- has-deep-value.js
+++ has-deep-value.js
@@ -1,1 +1,1 @@
-module.exports={hasDeepValue:(o,k)=>k.split(".").reduce((a,c)=>Object.hasOwnProperty.bind(a)(c)?a[c]||1:false,Object.assign({},o))===false?false:true,has:(k)=>(o)=>module.exports.hasDeepValue(o,k)}
+module.exports={hasDeepValue:(o,k)=>!k.split(".").reduce((a,c)=>Object.hasOwnProperty.bind(a)(c)?a[c]||1:!-1,Object.assign({},o))===!-1,has:k=>o=>module.exports.hasDeepValue(o,k)}
